bc21279ba443c6da55ee4745da4c27d1bea64b57
Miguellongoria <38727906+Miguellongoria@users.noreply.github.com>
2018-06-14 13:25:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Create("juanito", "manzana");
+            Sub(109, 40);
+            String result = Names("juanito", "manzana");
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+
+        // Using public
+        public static void Create(String a, String b)
+        {
+
+            Console.WriteLine(a + b);
+
+        }
+
+        //using return
+        public static String Names(String c, String d)
+        {
+            return c + d;
+        }
+
+        //using private
+        private static void Sub(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+
+
+
+    }
+}
